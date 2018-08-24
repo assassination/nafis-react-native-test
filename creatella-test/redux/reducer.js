96ae12fcb1a7252afb6ad4c623b1f67c50ad192b
Rachmad <rachmadnafisholeh@gmail.com>
2018-08-24 10:33:26 +0700

    fix bug, reducer not called after product is loaded

--- creatella-test/redux/reducer.js
+++ creatella-test/redux/reducer.js
@@ -1,14 +1,14 @@
 export const GET_BATCH_PRODUCT = 'nafis-react-native-test/product/GET_BATCH_PRODUCT'
-export const GET_PRODUCT_OK = 'nafis-react-native-test/product/GET_PRODUCT_OK'
-export const GET_PRODUCT_FAIL = 'nafis-react-native-test/product/GET_PRODUCT_FAIL'
+export const GET_BATCH_PRODUCT_SUCCESS = 'nafis-react-native-test/product/GET_BATCH_PRODUCT_SUCCESS'
+export const GET_BATCH_PRODUCT_FAIL = 'nafis-react-native-test/product/GET_BATCH_PRODUCT_FAIL'
 
 export default function reducer(state = { product: [] }, action) {
   switch(action.type) {
     case GET_BATCH_PRODUCT:       // on start batch load the product
       return { ...state, is_fetching: true }
-    case GET_PRODUCT_OK:          // on success batch loading the product
+    case GET_BATCH_PRODUCT_SUCCESS:          // on success batch loading the product
       return { ...state, is_fetching: false, data: action.payload.data }
-    case GET_PRODUCT_FAIL:        // on failed batch loading the product
+    case GET_BATCH_PRODUCT_FAIL:        // on failed batch loading the product
       return { ...state, is_fetching: false, error: 'error fetching product' }
     default:
       return state
@@ -21,7 +21,7 @@ export function getBatchProduct() {
     type: GET_BATCH_PRODUCT,
     payload: {
       request: {
-        url: ''
+        url: 'products'
       }
     }
   }
