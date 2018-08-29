583b156d9a0585e5e41968b8e0fcbdfb681ecd2f
Rachmad <rachmadnafisholeh@gmail.com>
2018-08-29 22:01:56 +0700

    normalize format of dollars, always display two numbers behind comma

--- creatella-test/utils/Number.js
+++ creatella-test/utils/Number.js
@@ -7,6 +7,13 @@ export function centToDollar(input) {
   } else if(input === '0' || input === 0) {   // cent value is zero, then convert to 'Free'
     return 'Free'
   } else {                                    // cent format is correct, then convert accordingly
-    return '$' + input / 100
+    let output = '$' + (input / 100)
+
+    // normalize dollar format, keeps two numbers behind comma
+    let n = output.split('.')
+    if(isNil(n[1])) output = output + '.00'
+    else if(n[1].length === 1) output = output + '0'  
+
+    return output
   }
 }
